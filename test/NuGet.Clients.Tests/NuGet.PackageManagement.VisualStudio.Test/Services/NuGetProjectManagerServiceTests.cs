93ad2085e2638f40a9695ffbd926cf342e7f6894
Nikolche Kolev <nikolev@microsoft.com>
2021-09-01 18:07:41 -0700

    Add GetSolutionDirectoryAsync to IVsSolutionManager

--- test/NuGet.Clients.Tests/NuGet.PackageManagement.VisualStudio.Test/Services/NuGetProjectManagerServiceTests.cs
+++ test/NuGet.Clients.Tests/NuGet.PackageManagement.VisualStudio.Test/Services/NuGetProjectManagerServiceTests.cs
@@ -793,6 +793,11 @@ namespace NuGet.PackageManagement.VisualStudio.Test
             {
                 throw new NotImplementedException();
             }
+
+            public Task<string> GetSolutionDirectoryAsync()
+            {
+                return Task.FromResult(_directory.Path);
+            }
         }
     }
 }
